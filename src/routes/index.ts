a4b0beadec7af5f67be41383572e44a7507f4970
Weslley Rodrigues <weslley.rdgs@gmail.com>
2024-12-16 19:49:47 -0300

    feat: rota privada

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { helloWorld } from '../controllers/helloworld.controller';
+import { auth } from '../middlewares/auth'
 
 const route = Router();
 
-route.get('/', helloWorld);
+route.get('/', auth, helloWorld);
 
 export default route;
